64b8c4925f0e9485a31b0e77ff28812585fb0dd5
Brooke Kullberg <brookekullberg@gmail.com>
2022-09-07 21:45:08 -0700

    add working project

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,60 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/theme-context";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  const theme = useContext(ThemeContext);
+  
+  // if (!theme) {
+  //   throw new Error("ThemeContext must be used within a ThemeContext.Provider!");
+  // }
+
+  const buttonStyles = { 
+    backgroundColor: theme.buttonBackground, 
+    color: theme.textColor, 
+  }
+
+  const inputStyles = { 
+    backgroundColor: theme.inputBackground,
+    color: theme.textColor, 
+  }
+  
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+      <label>Pair Names:
+          <br/>
+          <input
+            style={inputStyles}
+            type='text'
+            name='names' />
+        </label>
+        <br/>
+        <label>Location:
+          <br/>
+          <input
+            style={inputStyles}
+            type='text'
+            name='location' />
+        </label>
+        <br/>
+        <label>Describe your issue:
+          <br/>
+          <textarea
+            style={inputStyles}
+            name='issue' />
+        </label>
+        <br/>
+        <button style={buttonStyles}  type='submit'>{props.buttonText}</button>
+      </form>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
---
+++ src/components/TicketControl.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+import EditTicketForm from './EditTicketForm';
+import TicketDetail from './TicketDetail';
+import { ThemeContext } from "../context/theme-context";
+
+class TicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      mainTicketList: [],
+      selectedTicket: null,
+      editing: false
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.selectedTicket != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTicket: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  }
+
+  handleDeletingTicket = (id) => {
+    const newMainTicketList = this.state.mainTicketList.filter(ticket => ticket.id !== id);
+    this.setState({
+      mainTicketList: newMainTicketList,
+      selectedTicket: null
+    });
+  }
+
+  handleEditClick = () => {
+    this.setState({editing: true});
+  }
+
+  handleEditingTicketInList = (ticketToEdit) => {
+    const editedMainTicketList = this.state.mainTicketList
+      .filter(ticket => ticket.id !== this.state.selectedTicket.id)
+      .concat(ticketToEdit);
+    this.setState({
+      mainTicketList: editedMainTicketList,
+      editing: false,
+      selectedTicket: null
+    });
+  }
+
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({mainTicketList: newMainTicketList});
+    this.setState({formVisibleOnPage: false});
+  }
+
+  handleChangingSelectedTicket = (id) => {
+    const selectedTicket = this.state.mainTicketList.filter(ticket => ticket.id === id)[0];
+    this.setState({selectedTicket: selectedTicket});
+  }
+
+  render(){
+    let theme = this.context;
+
+    if (!theme) {
+      throw new Error("ThemeContext must be used within a ThemeContext.Provider!");
+    }
+  
+    const buttonStyles = { 
+      backgroundColor: theme.buttonBackground, 
+      color: theme.textColor, 
+    }
+
+    let currentlyVisibleState = null;
+    let buttonText = null; 
+    if (this.state.editing ) {      
+      currentlyVisibleState = <EditTicketForm ticket = {this.state.selectedTicket} onEditTicket = {this.handleEditingTicketInList} />
+      buttonText = "Return to Ticket List";
+    } else if (this.state.selectedTicket != null) {
+      currentlyVisibleState = <TicketDetail 
+      ticket={this.state.selectedTicket} 
+      onClickingDelete={this.handleDeletingTicket}
+      onClickingEdit = {this.handleEditClick} />
+      buttonText = "Return to Ticket List";
+    } else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}/>;
+      buttonText = "Return to Ticket List"; 
+    } else {
+      currentlyVisibleState = <TicketList onTicketSelection={this.handleChangingSelectedTicket} ticketList={this.state.mainTicketList} />;
+      buttonText = "Add Ticket"; 
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button style={buttonStyles} onClick={this.handleClick}>{buttonText}</button> 
+      </React.Fragment>
+    );
+  }
+
+}
+
+TicketControl.contextType = ThemeContext;
+
+export default TicketControl;
+
